477f7496d817d2639e2b9b5c33ac0ecac93b97a3
Samuel Magno <samuelrtfmagno@gmail.com>
2022-06-16 20:41:33 -0300

    Add client code

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 
 require __DIR__. '/vendor/autoload.php';
 
+use src\SortContext;
+use src\SortAsc;
+use src\SortDesc;
+use src\SortStrategy;
+
 echo "Strategy Example";
+
+$data = ['4,3,2,5,6,1'];
+
+$sort = new SortContext(new SortAsc());
+
+var_dump($sort->sortData($data));
